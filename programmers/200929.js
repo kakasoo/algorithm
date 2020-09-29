cb691ef557087d2994da21c9aa2acb09ca0e21cd
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-09-29 13:43:44 +0900

    프로그래머스 레벨1 소수찾기, 한줄평

--- programmers/200929.js
+++ programmers/200929.js
@@ -1,15 +1,19 @@
 function solution(n) {
-    let arr = [0, 0];
-    for (let i = 2; i <= n; i++) {
-        arr[i] = i;
-    }
+  // 몰랐던 사실
+  /*
+   *   let arr = []가 let arr = new Array(n) 보다 낫다.
+   */
+  let arr = [0, 0];
+  for (let i = 2; i <= n; i++) {
+    arr[i] = i;
+  }
 
-    for (let i = 2; i <= n; i++) {
-        if (arr[i] === 0) continue;
-        for (let j = i + i; j <= n; j += i) {
-            arr[j] = 0;
-        }
+  for (let i = 2; i <= n; i++) {
+    if (arr[i] === 0) continue;
+    for (let j = i + i; j <= n; j += i) {
+      arr[j] = 0;
     }
+  }
 
-    return arr.filter(el => el !== 0).length;
+  return arr.filter((el) => el !== 0).length;
 }
