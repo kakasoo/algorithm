f823d93c8c4225f98e256d0f4a5ad9b91dd40f87
kakasoo <55487286+kakamura@users.noreply.github.com>
2020-09-29 14:50:03 +0900

    프로그래머스 레벨1 시저 암호를 풀었습니다.

---
+++ programmers/200929-2.js
@@ -0,0 +1,22 @@
+function solution(s, n) {
+  var answer = "";
+  let big = [];
+  let small = [];
+  for (let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++)
+    big.push(String.fromCharCode(i));
+  for (let i = "a".charCodeAt(0); i <= "z".charCodeAt(0); i++)
+    small.push(String.fromCharCode(i));
+
+  s = s.split("").map((el, i, arr) => {
+    if (big.includes(el)) {
+      let nextPos = big.indexOf(el) + n;
+      if (nextPos > big.length - 1) nextPos -= big.length;
+      return big[nextPos];
+    } else if (small.includes(el)) {
+      let nextPos = small.indexOf(el) + n;
+      if (nextPos > small.length - 1) nextPos -= small.length;
+      return small[nextPos];
+    } else return el;
+  });
+  return s.join("");
+}
