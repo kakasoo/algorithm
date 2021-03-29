5c6fc1f9546d14bb38889425ac480767c2bca3a5
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-03-29 18:20:16 +0900

    백준 2165번 포도주 시식을 풀었습니다
    - 자바스크립트에서는 타입이 없어서 문제가 생길 수 있다...
    - 다른 언어와 달리 버그를 찾기가 쉽지 않다.
    - for 문에 number가 아닌 경우를 찾는 게 그 예시다.

---
+++ baekjoon/BJ2165.js
@@ -0,0 +1,47 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let count = 0;
+const input = [0];
+
+rl.on("line", (line) => {
+    if (!count) {
+        count = Number(line);
+    } else {
+        input.push(Number(line));
+        if (input.length === count + 1) {
+            main();
+            rl.close();
+        }
+    }
+}).on("close", () => {
+    process.exit();
+});
+
+const main = () => {
+    const DP = new Array(count + 1).fill(0);
+    DP[1] = input[1];
+    DP[2] = input[2] + DP[1];
+    DP[3] = Math.max(DP[1] + input[3], input[2] + input[3]);
+
+    for (let i = 1; i <= 3; i++) {
+        if (isNaN(DP[i])) {
+            DP[i] = 0;
+        }
+    }
+
+    for (let i = 4; i <= count; i++) {
+        DP[i] = Math.max(
+            input[i] + DP[i - 2],
+            input[i] + input[i - 1] + DP[i - 3],
+            input[i] + input[i - 1] + DP[i - 4],
+            DP[i - 1]
+        );
+    }
+
+    console.log(Math.max(...DP));
+};
