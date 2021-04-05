becf796903a782b22ab026679d9b427476b5f0b2
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-04-05 10:59:10 +0900

    백준 14501번 퇴사 문제를 풀었습니다.

---
+++ baekjoon/BJ14501.js
@@ -0,0 +1,60 @@
+// 백준 14501번 퇴사 문제를 풀었습니다.
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let count = 0;
+const input = [];
+
+rl.on("line", (line) => {
+    if (count === 0) {
+        count = Number(line);
+    } else {
+        input.push(line);
+        if (input.length === count) {
+            main();
+            rl.close();
+        }
+    }
+}).on("close", () => {
+    process.exit();
+});
+
+const main = () => {
+    const T = [0];
+    const P = [0];
+
+    input.forEach((el, i) => {
+        numbers = el.split(" ").map(Number);
+        const day = numbers[0];
+        const price = numbers[1];
+
+        T.push(day);
+        P.push(price);
+    });
+
+    const DP = new Array(T.length).fill(0);
+    DP[1] = P[1];
+
+    for (let i = 2; i < DP.length; i++) {
+        DP[i] = P[i];
+
+        if (i + T[i] - 1 > count) {
+            DP[i] = 0;
+            continue;
+        }
+
+        let temp = DP[i];
+        for (let j = 1; j < i; j++) {
+            if (j + T[j] <= i) {
+                temp = Math.max(temp, DP[j] + P[i]);
+            }
+        }
+        DP[i] = temp;
+    }
+    console.log(Math.max(...DP.slice(1, count + 1)));
+};
