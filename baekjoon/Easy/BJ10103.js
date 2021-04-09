7720a37eebd34cdd429d17d34be0562228474ef7
kakasoo <55487286+kakamura@users.noreply.github.com>
2021-04-09 18:04:23 +0900

    백준 10103번 주사위 게임을 풀었습니다.

---
+++ baekjoon/Easy/BJ10103.js
@@ -0,0 +1,39 @@
+// 백준 10103번 주사위 게임을 풀었습니다.
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let count = 0;
+const input = [];
+rl.on("line", (line) => {
+    if (!count) {
+        count = Number(line);
+    } else {
+        input.push(line);
+        if (input.length === count) {
+            main();
+            rl.close();
+        }
+    }
+}).on("close", () => {
+    process.exit();
+});
+
+const main = () => {
+    let chang = 100;
+    let sang = 100;
+
+    input.forEach((el) => {
+        const [a, b] = el.split(" ").map(Number);
+        if (a > b) {
+            sang -= a;
+        } else if (a < b) {
+            chang -= b;
+        }
+    });
+    console.log(chang);
+    console.log(sang);
+};
