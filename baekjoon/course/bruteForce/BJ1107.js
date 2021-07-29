23ad1e8c4dfb34ee8dc301a4918ff497f17681e0
kakasoo <kscodebase@gmail.com>
2021-07-29 16:34:56 +0900

    백준 1107번 리모컨을 풀었습니다.

--- baekjoon/course/bruteForce/BJ1107.js
+++ baekjoon/course/bruteForce/BJ1107.js
@@ -1,13 +1,5 @@
 const readline = require("readline");
 
-// down이 up보다 먼저여야 한다.
-// down과 up이 동시에 도달하는 지점에서, down의 자릿수가 up보다 작을 수 있기 때문
-
-// down은 마이너스가 될 수도 있기 때문에 체크해준다.
-
-// toString을 써서 숫자를 문자열로 바꾼 다음, split 하여 망가진 버튼이 포함되나 체크할 경우,
-// 마이너스에 유의한다.
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -17,10 +9,8 @@ const input = [];
 
 rl.on("line", (line) => {
     input.push(line);
-
-    if (input.length === 3) {
-        main();
-    }
+}).on("close", () => {
+    main();
 });
 
 /**
@@ -46,10 +36,12 @@ const main = () => {
         return;
     }
 
-    const brokenButtons = input[2]
-        .split(" ")
-        .filter((el) => !!el)
-        .map(Number);
+    const brokenButtons = Number(input[1])
+        ? input[2]
+              .split(" ")
+              .filter((el) => !!el)
+              .map(Number)
+        : [];
 
     let upDownCount = 0;
     let downNumber = target;
